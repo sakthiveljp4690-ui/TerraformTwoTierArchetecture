922b2338e5e4339c84cd573db7e184067c8f0d00
“sakthivel.venkatesan” <sakthiveljp4960@gmail.com>
2026-03-03 11:33:20 +0900

    adding readme

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,27 +8,6 @@ data "aws_ami" "amazon_linux" {
     }
 }
 
-# resource "aws_instance" "private_instance" {
-#     ami = data.aws_ami.amazon_linux.id
-#     instance_type = var.instance_type
-#     iam_instance_profile = var.ec2_instance_profile
-#     vpc_security_group_ids = [var.private_ec2_sg]
-#     subnet_id = var.private_subnet_az1
-#     associate_public_ip_address = false
-#     user_data = <<-EOF
-#               #!/bin/bash
-#               sudo yum update -y
-#               sudo amazon-linux-extras install nginx1
-#               sudo systemctl start nginx
-#               sudo systemctl enable nginx
-#               sudo yum install mysql -y
-#               sudo curl -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-#               EOF
-#     tags = {
-#         Name = "terraform-application-private"
-#     }
-# }
-
 resource "aws_launch_template" "private_instance_template" {
     name = "private_instance_template"
     image_id = data.aws_ami.amazon_linux.id
@@ -51,7 +30,7 @@ resource "aws_launch_template" "private_instance_template" {
 resource "aws_autoscaling_group" "ec2_asg" {
     name = "ec2_asg"
     max_size = 3
-    min_size = 1
+    min_size = 2
     vpc_zone_identifier = [var.private_subnet_az1, var.private_subnet_az2 ]
     target_group_arns = [var.alb_tgs]
     launch_template {
